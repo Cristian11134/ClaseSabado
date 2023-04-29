37974318ba96a1a50aea24f8365efe4c18255788
cristian11134 <cristian.oviedosoler@gmail.com>
2023-04-29 00:16:33 -0500

    clase sabado

---
+++ productos.php
@@ -0,0 +1,62 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+
+<div class="card">
+
+<img class="card-img-top" src="holder.js/100x180/" alt="">
+
+<div class="card-body">
+        <h4 class="card-title">Ropa php</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver</a>
+</div>
+
+</div>
+
+</div>
+<div class="col-md-3">
+
+<div class="card">
+
+<img class="card-img-top" src="holder.js/100x180/" alt="">
+
+<div class="card-body">
+        <h4 class="card-title">Ropa php</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver</a>
+</div>
+
+</div>
+
+</div>
+<div class="col-md-3">
+
+<div class="card">
+
+<img class="card-img-top" src="holder.js/100x180/" alt="">
+
+<div class="card-body">
+        <h4 class="card-title">Ropa php</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver</a>
+</div>
+
+</div>
+
+</div>
+<div class="col-md-3">
+
+<div class="card">
+
+<img class="card-img-top" src="holder.js/100x180/" alt="">
+
+<div class="card-body">
+        <h4 class="card-title">Ropa php</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver</a>
+</div>
+
+</div>
+
+</div>
+
+
+
+<?php include("template/pie.php"); ?>
